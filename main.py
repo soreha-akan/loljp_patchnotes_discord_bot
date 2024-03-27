e5cd6a038651649abfd6f3af0f9c3a5e93c13f9f
searide <hapimayca@gmail.com>
2024-03-28 05:39:50 +0900

    マルチサーバー対応しました

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import discord
 import io
 import json
 import os
+import pytz
 from bs4 import BeautifulSoup
+from datetime import datetime
 from discord import Intents, Client
 from discord.app_commands import CommandTree
 from discord.ext import tasks
 from urllib.parse import urljoin
 from keep_alive import keep_alive
 from google.cloud import storage
+
+jst = pytz.timezone('Asia/Tokyo')
 
 intents = Intents.default()
 intents.typing = False
@@ -24,14 +28,14 @@ tree = CommandTree(bot)
 
 bucket_name = 'loljp-discord-bot'
 bucket = storage_client.get_bucket(bucket_name)
-patch_title_json_path = 'product/last_patch_title.json'
-dev_title_json_path = 'product/last_dev_title.json'
+patch_info_json_path = 'product/last_patch_info.json'
+dev_info_json_path = 'product/recent_dev_info.json'
 guild_list_json_path = 'product/guild_list.json'
 
-is_develop = True
+is_develop = False
 if is_develop:
-    patch_title_json_path = 'develop/last_patch_title.json'
-    dev_title_json_path = 'develop/last_dev_title.json'
+    patch_info_json_path = 'develop/last_patch_info.json'
+    dev_info_json_path = 'develop/recent_dev_info.json'
     guild_list_json_path = 'develop/guild_list.json'
 
 # GCS上のjsonファイルからサーバーリストを取得する
@@ -42,14 +46,11 @@ def load_guild_list():
     return data
 
 # GCS上のjsonファイルから直近の更新内容を取得する
-def load_last_titles(json_path):
+def load_last_info_json(json_path):
     blob = bucket.blob(json_path)
     content = blob.download_as_text()
 
-    data = json.loads(content)
-    if json_path == patch_title_json_path:
-        data = data.get("last_patch_title", "")
-    return data
+    return json.loads(content)
 
 # GCS上のjsonファイルにサーバーリストを保存する
 def save_guild_list(guild_list):
@@ -87,8 +88,8 @@ def get_enabled_channel_id_list(guild_list):
 @bot.event
 async def on_ready():
     print(f"{bot.user}のログインに成功！")
-    check_patch_title.start()
-    check_dev_title.start()
+    check_patch_update.start()
+    check_dev_update.start()
     await tree.sync()
 
 @bot.event
@@ -225,9 +226,30 @@ async def start_command(ctx):
     await ctx.response.send_message(message)
 
 @tasks.loop(minutes=15)
-async def check_patch_title():
-    last_patch_title = load_last_titles(patch_title_json_path)
+async def check_patch_update():
+    last_patch_info_json = load_last_info_json(patch_info_json_path)
+    last_patch_title = last_patch_info_json["title"]
+    last_patch_url = last_patch_info_json["url"]
 
+    patch_title, patch_url = await scrape_patch_link_list()
+
+    if patch_title != last_patch_title and patch_url != last_patch_url:
+        patch_img = await get_patch_img(patch_url)
+        if patch_img is not None:
+            # 画像をメッセージに添付して送信
+            await send_patch_message(patch_title, patch_url, patch_img)
+        else:
+            await send_patch_message(patch_title, patch_url)
+
+        new_patch_info_json = {
+            "title": patch_title,
+            "url": patch_url,
+            "modified": datetime.now(jst).strftime("%Y-%m-%d %H:%M:%S")
+        }
+        # 新しいパッチ情報をファイルに保存
+        save_last_titles(new_patch_info_json, patch_info_json_path)
+
+async def scrape_patch_link_list():
     print(f"{bot.user} - パッチタイトルチェック開始")
     url = "https://www.leagueoflegends.com/ja-jp/news/tags/patch-notes/"
     async with aiohttp.ClientSession() as session:
@@ -242,21 +264,26 @@ async def check_patch_title():
 
     if target_a_tags:
         first_target_a_tag = target_a_tags[0]
-        patch_url = first_target_a_tag.get("href")
-        patch_full_url = urljoin(url, patch_url)
+        h2_element = first_target_a_tag.find(
+            "h2", class_="style__Title-sc-1h41bzo-8 hvOSAW"
+        )
 
-        # full_url のリンク先ページを取得
+        patch_title = h2_element.text
+        href = first_target_a_tag.get("href")
+        patch_url = urljoin(url, href)
+
+        return patch_title, patch_url
+
+async def get_patch_img(patch_url):
         async with aiohttp.ClientSession() as session:
-            async with session.get(patch_full_url) as response:
+            async with session.get(patch_url) as response:
                 page_html = await response.text()
 
-        # "Patch-Highlights_TW_1920x1080_JA.jpgを含む画像URLを取得
         page_soup = BeautifulSoup(page_html, "html.parser")
 
         img_tags = page_soup.find_all("img", src=True)
         target_src = "Patch-Highlights_TW_1920x1080_JA"
         match_string_img = [img["src"] for img in img_tags if target_src in img["src"]]
-
         cbox_class_img = page_soup.find("a", class_="skins cboxElement")
 
         img_url = None
@@ -265,44 +292,60 @@ async def check_patch_title():
         elif cbox_class_img:
             img_url = cbox_class_img["href"]
 
-        # <h2>要素を取得
-        h2_element = first_target_a_tag.find(
-            "h2", class_="style__Title-sc-1h41bzo-8 hvOSAW"
-        )
+        image_data = None
+        if img_url is not None:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(img_url) as image_response:
+                    image_data = await image_response.read()
 
-        patch_title = h2_element.text
+        return image_data
 
-        if patch_title != last_patch_title:
-            channel = bot.get_channel(channel_id)  # パッチ情報を送信するチャンネルのIDを指定
-
-            if img_url is not None:
-                # 画像ファイルのURLから画像をダウンロード
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(img_url) as image_response:
-                        image_data = await image_response.read()
-
-                # 画像をメッセージに添付して送信
-                image_file = discord.File(
-                    io.BytesIO(image_data), filename="patch_hilight_image.png"
-                )
-                await channel.send(
-                    f"### - [{patch_title}](<{patch_full_url}>)", file=image_file
-                )
-            else:
-                await channel.send(f"### - [{patch_title}](<{patch_full_url}>)")
-
-            title_json = {"last_patch_title": patch_title}
-            # 新しいパッチタイトルをファイルに保存
-            save_last_titles(title_json, patch_title_json_path)
-
-@check_patch_title.before_loop
-async def before_check_patch_title():
+async def send_patch_message(patch_title, patch_url, patch_img=None):
+    guild_list = load_guild_list()
+    enabled_channel_list = get_enabled_channel_id_list(guild_list)
+    if patch_img is not None:
+        for channel_id in enabled_channel_list:
+            channel = bot.get_channel(channel_id)   
+            image_file = discord.File(
+                io.BytesIO(patch_img), filename="patch_hilight_image.png"
+            )
+            await channel.send(
+                f"### - [{patch_title}](<{patch_url}>)", file=image_file
+            )
+    else:
+        for channel_id in enabled_channel_list:
+            channel = bot.get_channel(channel_id)
+            await channel.send(f"### - [{patch_title}](<{patch_url}>)")
+        
+@check_patch_update.before_loop
+async def before_check_patch_update():
     await bot.wait_until_ready()
 
 @tasks.loop(minutes=15)
-async def check_dev_title():
-    last_dev_titles = load_last_titles(dev_title_json_path)
-    
+async def check_dev_update():
+    recent_dev_info = load_last_info_json(dev_info_json_path)
+
+    new_dev_info_list = []
+    articles = await scrape_dev_link_list()
+    for article in articles:
+        if check_dev_is_new(article, recent_dev_info):
+            new_dev_info = {
+                "title": article["title"],
+                "url": article["url"],
+                "modified": datetime.now(jst).strftime("%Y-%m-%d %H:%M:%S")
+            }
+            new_dev_info_list.append(new_dev_info)
+    if len(new_dev_info_list) > 0:
+        await send_dev_message(new_dev_info_list)
+                
+        # recent_dev_infoの末尾にtitleを追加
+        recent_dev_info = new_dev_info_list + recent_dev_info
+        if len(recent_dev_info) > 20:
+            recent_dev_info = recent_dev_info[:20]
+
+        save_last_titles(recent_dev_info, dev_info_json_path)
+
+async def scrape_dev_link_list():
     print(f"{bot.user} - Devタイトルチェック開始")
     url = "https://www.leagueoflegends.com/ja-jp/news/dev/"
     async with aiohttp.ClientSession() as session:
@@ -315,35 +358,38 @@ async def check_dev_title():
         class_="style__Wrapper-sc-1h41bzo-0 style__ResponsiveWrapper-sc-1h41bzo-13 eIUhoC cGAodJ isVisible",
     )
 
-    titles = {}
+    articles = []
     for target_a_tag in target_a_tags:
-        dev_url = target_a_tag.get("href")
-        dev_full_url = urljoin(url, dev_url)
+        href = target_a_tag.get("href")
+        dev_url = urljoin(url, href)
 
         # <h2>要素を取得
         h2_element = target_a_tag.find("h2", class_="style__Title-sc-1h41bzo-8 hvOSAW")
 
-        titles[h2_element.text] = dev_full_url
+        article = {
+            "title": h2_element.text,
+            "url": dev_url
+        }
+        articles.append(article)
 
-    for key, value in titles.items():
-        if key not in last_dev_titles:
-            print("Dev titles were updated")
+    return articles
+
+def check_dev_is_new(article, recent_dev_info):
+    for dev_info in recent_dev_info:
+        if article["url"] == dev_info["url"]:
+            return False
+    return True
+
+async def send_dev_message(new_dev_info_list):
+    guild_list = load_guild_list()
+    enabled_channel_list = get_enabled_channel_id_list(guild_list)
+    for new_dev_info in new_dev_info_list:
+        for channel_id in enabled_channel_list:
             channel = bot.get_channel(channel_id)
-            # メッセージを送信
-            await channel.send(f"### - [{key}]({value})")
-            # last_dev_titlesの末尾にtitleを追加
-            last_dev_titles_list = list(last_dev_titles.items())
-            last_dev_titles_list.append((key, value))
-
-            if len(last_dev_titles_list) > 20:
-                last_dev_titles_list = last_dev_titles_list[-20:]
-
-            # 辞書に変換して保存
-            title_json = dict(last_dev_titles_list)
-            save_last_titles(title_json, dev_title_json_path)
-
-@check_dev_title.before_loop
-async def before_check_dev_title():
+            await channel.send(f"### - [{new_dev_info['title']}]({new_dev_info['url']})")
+                
+@check_dev_update.before_loop
+async def before_check_dev_update():
     await bot.wait_until_ready()
 
 try:
