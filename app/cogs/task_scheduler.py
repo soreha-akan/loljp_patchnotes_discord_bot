5919cf11479adbb7cce300118e3c224b71086133
soreha-akan <hapimayca@gmail.com>
2025-05-14 06:54:31 +0900

    タスクループの開始を遅延

--- app/cogs/task_scheduler.py
+++ app/cogs/task_scheduler.py
@@ -1,6 +1,6 @@
+import discord
 from discord.ext import commands, tasks
 from Service.CheckUpdateService import CheckUpdateService
-
 
 class TaskScheduler(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -10,7 +10,16 @@ class TaskScheduler(commands.Cog):
 
     async def cog_unload(self):
         self.check_updates_loop.cancel()
-        
+
     @tasks.loop(minutes=15)
     async def check_updates_loop(self):
         await self.check_update_service.check_updates()
+
+    @check_updates_loop.before_loop
+    async def before_check_updates(self):
+        await self.bot.wait_until_ready()
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        if not self.check_updates_loop.is_running():
+            self.check_updates_loop.start()
